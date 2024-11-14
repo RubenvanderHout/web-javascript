86e5801f85f9cdfcb364b35041c35a28fa9ac9af
RadicalRumin <merelenjoy66@gmail.com>
2024-11-14 13:31:19 +0100

    omg soooo random

--- v2/src/components/RandomIngredient.js
+++ v2/src/components/RandomIngredient.js
@@ -7,8 +7,41 @@ export function RandomIngredientComponent() {
 
     function generateProperties() {
         const color = generateHSL();
+        const texture = generateTexture();
+        const mixingTime = generateMixingTime();
+        const mixingSpeed = generateMixingSpeed();
+        const shape = generateShape();
+        
+
+        return {
+            color,
+            texture,
+            mixingTime,
+            mixingSpeed,
+            shape
+        };
 
     }
 
-    return IngredientComponent();
+    return IngredientComponent(generateProperties());
+}
+
+function generateTexture() {
+    const textures = ['grain', 'coarse grain', 'smooth', 'slimy'];
+    const randomIndex = Math.floor(Math.random() * textures.length);
+    return textures[randomIndex];
+}
+
+function generateMixingTime() {
+    return (Math.floor(Math.random() * 4) + 1) * 1000;
+}
+
+function generateMixingSpeed() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+function generateShape() {
+    const shapes = ['square', 'circle', 'triangle', 'hexagon'];
+    const randomIndex = Math.floor(Math.random() * shapes.length);
+    return shapes[randomIndex];
 }
