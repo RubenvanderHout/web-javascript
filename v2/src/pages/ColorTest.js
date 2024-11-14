f4d17fec5eef5ec322099ff9b9d5db9e8bffa79c
RadicalRumin <merelenjoy66@gmail.com>
2024-11-14 15:59:07 +0100

    non functiong color test grid

--- v2/src/pages/ColorTest.js
+++ v2/src/pages/ColorTest.js
@@ -1,7 +1,17 @@
 export function ColorTestPage() {
+    const gridSize = 5; // Define the size of the grid (5x5)
+    let squaresHtml = '';
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        squaresHtml += `<div class="droppable colortest" id="square-${i}"></div>`;
+    }
+
     const html = `
-        <div>
+        <div class="color-test-page">
             <h1>Color Test!</h1>
+            <div class="grid-container">
+                ${squaresHtml}
+            </div>
         </div>
     `;
     const range = document.createRange();
