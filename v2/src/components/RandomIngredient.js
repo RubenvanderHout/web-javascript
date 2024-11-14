359ab836839502b09827852c233b728fa7bf5c58
RadicalRumin <merelenjoy66@gmail.com>
2024-11-14 13:05:49 +0100

    camelCase

--- v2/src/components/RandomIngredient.js
+++ v2/src/components/RandomIngredient.js
@@ -1,10 +1,12 @@
 // @ts-nocheck
 import { generateRandomId } from "../utils/utils.js";
 import { IngredientComponent } from "./Ingredient.js";
+import { generateHSL } from "../utils/colors.js";
 
 export function RandomIngredientComponent() {
 
     function generateProperties() {
+        const color = generateHSL();
 
     }
 
