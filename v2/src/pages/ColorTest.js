80e1d6e92bd039051308d7bb6e53f0e025a4f587
RadicalRumin <merelenjoy66@gmail.com>
2024-11-19 13:59:12 +0100

    Color swatch gen

--- v2/src/pages/ColorTest.js
+++ v2/src/pages/ColorTest.js
@@ -1,21 +1,57 @@
 export function ColorTestPage() {
-    const gridSize = 5; // Define the size of the grid (5x5)
-    let squaresHtml = '';
-
-    for (let i = 0; i < gridSize * gridSize; i++) {
-        squaresHtml += `<div class="droppable colortest" id="square-${i}"></div>`;
-    }
+    
 
     const html = `
         <div class="color-test-page">
             <h1>Color Test!</h1>
-            <div class="grid-container">
-                ${squaresHtml}
+            <form id="generate-grid">
+                <input type="number" min="1" max="20" id="grid-x" placeholder="x">
+                <input type="number" min="1" max="20" id="grid-y" placeholder="y">
+                <input type="submit" value="Generate Grid">
+            </form>
+            <div id="color_swatch">
+                
             </div>
+            
+            <div class="centered">
+                <h2>Triadic Colors</h2>
+                <div class="flex-container">
+                    <div class="triadic-square" id="triadic-1"></div>
+                    <div class="triadic-square" id="triadic-2"></div>
+                    <div class="triadic-square" id="triadic-3"></div>
+                </div>
+            </div>
+        
         </div>
     `;
     const range = document.createRange();
     const fragment = range.createContextualFragment(html);
 
+    const button = fragment.querySelector('#generate-grid');
+    button.addEventListener('submit', generateGrid);
+
     return fragment;
+}
+
+function generateGrid() {
+    //@ts-ignore
+    const x = document.getElementById('grid-x').value;
+    //@ts-ignore
+    const y = document.getElementById('grid-y').value;
+
+    const parent = document.getElementById('color_swatch');
+    parent.innerHTML = '';
+    for (let i = 0; i < x; i++) {
+        let row = document.createElement('div');
+        row.classList.add('flex-container');
+        for (let j = 0; j < y; j++) {
+            // create square
+            const square = document.createElement('div');
+            square.classList.add('colortest');
+            row.appendChild(square);
+        }
+        parent.appendChild(row);
+        
+    }
+    
 }
